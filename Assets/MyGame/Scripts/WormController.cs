73eaa017c16b214aae3beea863a7c0ab2c41077c
Kilian Reisinger <kilian.reisinger02@htl-salzburg.ac.at>
2020-05-26 14:48:44 +0200

    instantiate projectile

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -5,32 +5,50 @@ using UnityEngine;
 public class WormController : MonoBehaviour
 {
     public KeyCode jumpKey;
+    public KeyCode downKey;
     public KeyCode fowardKey;
     public KeyCode backKey;
-    public Rigidbody rb;
-    public Vector3 x;
-    public Vector3 y;
+    public KeyCode shootKey;
+    public UnityEngine.Rigidbody rb;
+    public Vector3 vecJump;
+    public Vector3 vecForward;
+
+    public GameObject gun;
+    
     // Update is called once per frame
+    public GameObject projectile;
     void Update()
     {
         if (Input.GetKeyDown(jumpKey))
         {
             Debug.Log("Jump taste wurde gedrückt");
-            rb.AddForce(x);
+            rb.AddForce(vecJump);
         }
 
+        if (Input.GetKeyDown(downKey))
+        {
+            Debug.Log("Jump taste wurde gedrückt");
+            rb.AddForce(-vecJump);
+        }
         if (Input.GetKeyDown(fowardKey))
         {
             Debug.Log("Forwärts taste wurde gedrückt");
-            rb.AddForce(y);
-
+            rb.AddForce(vecForward);
         }
 
         if (Input.GetKeyDown(backKey))
         {
             Debug.Log("Zurück taste wurde gedrückt");
-            rb.AddForce(-y);
-
+            rb.AddForce(-vecForward);
         }
+
+
+        if (Input.GetKeyDown(shootKey))
+        {
+            // Vector3 gunpos = gun.transform.position;
+  
+            Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+        }
+
     }
 }
