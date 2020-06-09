fff078c3e3b2496b41a673213f7a97ef5bc8cb84
kilianreisinger <56028726+kilianreisinger@users.noreply.github.com>
2020-06-09 15:03:20 +0200

    shooting

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -12,6 +12,7 @@ public class WormController : MonoBehaviour
     public UnityEngine.Rigidbody rb;
     public Vector3 vecJump;
     public Vector3 vecForward;
+    public Vector3 shootforce;
 
     public GameObject gun;
     
@@ -19,25 +20,38 @@ public class WormController : MonoBehaviour
     public GameObject projectile;
     void Update()
     {
-        if (Input.GetKeyDown(jumpKey))
+        if (Input.GetKey(jumpKey))
         {
             Debug.Log("Jump taste wurde gedrückt");
             rb.AddForce(vecJump);
         }
 
-        if (Input.GetKeyDown(downKey))
+        if (Input.GetKey(downKey))
         {
             Debug.Log("Jump taste wurde gedrückt");
             rb.AddForce(-vecJump);
         }
-        if (Input.GetKeyDown(fowardKey))
+        if (Input.GetKey(fowardKey))
         {
+
+            if(!(this.gameObject.transform.rotation.y == 0f)) {
+                this.gameObject.transform.Rotate(0f, 180f,0f, Space.Self);
+            }
             Debug.Log("Forwärts taste wurde gedrückt");
             rb.AddForce(vecForward);
         }
 
-        if (Input.GetKeyDown(backKey))
+        if (Input.GetKey(backKey))
         {
+            // var Scale = this.gameObject.transform.localScale;
+            // Vector2 invert = new Vector3(-1,1,1);
+            // var flip = Scale * invert; 
+            // this.gameObject.transform.localScale = flip;
+
+      
+            if(!(this.gameObject.transform.rotation.y == 1f)) {
+                this.gameObject.transform.Rotate(0f, 180f,0f, Space.Self);
+            }
             Debug.Log("Zurück taste wurde gedrückt");
             rb.AddForce(-vecForward);
         }
@@ -47,7 +61,8 @@ public class WormController : MonoBehaviour
         {
             // Vector3 gunpos = gun.transform.position;
   
-            Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+            var shot = Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+            shot.GetComponent<Rigidbody>().AddForce(shootforce);
         }
 
     }
