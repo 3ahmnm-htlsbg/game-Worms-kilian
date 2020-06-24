1b72f643e604faf98e2c8683369c5091f65906e6
Kilian Reisinger <kilian.reisinger02@htl-salzburg.ac.at>
2020-06-24 22:48:28 +0200

    code cleanup

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -24,23 +24,13 @@ public class WormController : MonoBehaviour
     public GameObject projectile;
     void Update()
     {
-                // Bit shift the index of the layer (8) to get a bit mask
         int layerMask = 1 << 8;
-
-        // This would cast rays only against colliders in layer 8.
-        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
         layerMask = ~layerMask;
 
         RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(this.gameObject.transform.Find("ray").gameObject.transform.position, raydir, out hit, 0.2f))
         {
-            // Debug.Log("Did Hit");
-        
-        // Debug.DrawRay(this.gameObject.transform.Find("ray").gameObject.transform.position, raydir, Color.yellow);
-      
 
-        
             if (Input.GetKey(jumpKey))
             {
                 rb.AddForce(vecJump);
